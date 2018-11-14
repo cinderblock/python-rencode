1f76d9759c5b318dc32929c0e4398e8cae7baca9
Cameron Tacklind <cameron@tacklind.com>
2018-11-13 16:39:29 -0800

    Test decoding with bad argument

--- __tests__/decode.js
+++ __tests__/decode.js
@@ -11,3 +11,9 @@ test("Decode 'a' to be Buffer<'a'>", () => {
 test('Decode big number to be Error', () => {
   expect(() => decode(Buffer.from([65, 1, 0, 0, 0, 0, 0, 0, 0]))).toThrow('Encoded value outside of decodable range.');
 });
+
+test('Decode bad argument', () => {
+  const o = {};
+  o.length = -1;
+  expect(() => decode(o)).toThrow('Malformed rencoded string: data_length: ' + -1 + ' pos: ' + 0);
+});
