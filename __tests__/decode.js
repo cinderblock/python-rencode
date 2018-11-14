d36cfebab2973da75650e2a47120319e8ff1bba3
Cameron Tacklind <cameron@tacklind.com>
2018-11-13 16:29:26 -0800

    Test decoding long that is too big

--- __tests__/decode.js
+++ __tests__/decode.js
@@ -3,6 +3,11 @@ const { decode } = require('../rencode.js');
 test('Decode Buffer<1> to be 1', () => {
   expect(decode(Buffer.from([1]))).toEqual(1);
 });
+
 test("Decode 'a' to be Buffer<'a'>", () => {
   expect(decode(Buffer.from([129, 97]))).toEqual('a');
+});
+
+test('Decode big number to be Error', () => {
+  expect(() => decode(Buffer.from([65, 1, 0, 0, 0, 0, 0, 0, 0]))).toThrow('Encoded value outside of decodable range.');
 });
