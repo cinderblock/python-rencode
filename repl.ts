af50ecbf844dac418c0b08a78f89a2a2af52436a
Cameron Tacklind <cameron@tacklind.com>
2018-11-17 01:27:46 -0800

    repl to ts

--- repl.ts
+++ repl.ts
@@ -1,6 +1,6 @@
 const c = require('repl').start({ ignoreUndefined: true }).context;
 
-const { encode, decode } = require('./rencode.js');
+const { encode, decode } = require('./rencode');
 
 c.encode = encode;
 c.decode = decode;
