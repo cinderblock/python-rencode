a7fc1796030715b5abc2b27b69e1e4ade359f330
Cameron Tacklind <cameron@tacklind.com>
2018-11-18 02:39:30 -0800

    Rearrange folders

--- repl.ts
+++ repl.ts
@@ -1,6 +1,6 @@
 const c = require('repl').start({ ignoreUndefined: true }).context;
 
-const { encode, decode } = require('./rencode');
+const { encode, decode } = require('.');
 
 c.encode = encode;
 c.decode = decode;
